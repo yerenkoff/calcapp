4855c20af5dd8c0259d1dbdbdf3394492402ae1e
Kirill Yerenkov <yerenkov@Kirills-MacBook-Air.local>
2022-05-28 23:04:27 +0300

    Updates

---
+++ precache-manifest.bf880edb990c3060c78e743c0fe7f981.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = [
+  {
+    "revision": "996725df201c1e6d3f72",
+    "url": "/calcapp/static/js/main.996725df.chunk.js"
+  },
+  {
+    "revision": "cfda1b899bd53eb4481d",
+    "url": "/calcapp/static/js/1.cfda1b89.chunk.js"
+  },
+  {
+    "revision": "cf5428eedc7735fea312",
+    "url": "/calcapp/static/js/runtime~main.cf5428ee.js"
+  },
+  {
+    "revision": "5b7c19b72c23856a70239c6fb5e7f18a",
+    "url": "/calcapp/index.html"
+  }
+];
